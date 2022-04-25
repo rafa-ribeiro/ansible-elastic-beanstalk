0b059ebaebe249b4ff1615b0da7e4e712ee884e7
Rafael <tecinfo.rafael@gmail.com>
2022-04-25 14:49:33 -0300

    VDP-1308 - Corrige acesso ao field VersionLabel

--- library/elasticbeanstalk_env.py
+++ library/elasticbeanstalk_env.py
@@ -228,6 +228,9 @@ def describe_env_config_settings(ebs, app_name, env_name):
 
 def update_required(ebs, env, params):
     updates = []
+    if 'VersionLabel' not in env:
+        env['VersionLabel'] = None
+
     if params["version_label"] and env["VersionLabel"] != params["version_label"]:
         updates.append(('VersionLabel', env['VersionLabel'], params['version_label']))
 
